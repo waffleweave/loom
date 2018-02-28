38e63ddb37f30d9ffdfa72a8e9a02011e2568088
Isaac Goldthwaite <isaac.goldthwaite@hotmail.com>
2018-02-27 20:46:53 -0500

    attempt fix build error

--- src/extension.ts
+++ src/extension.ts
@@ -4,13 +4,17 @@ import { WeaveSearcher } from './weavesearcher';
 
 // Isaac cannot hit watson services without this (at the moment)...
 // this is probably a bad idea though so only enable this if you have to.
-require('https').globalAgent.options.rejectUnauthorized = false;
+// require('https').globalAgent.options.rejectUnauthorized = false;
 
 export function activate(context: ExtensionContext) {
     let weaveSearcher = new WeaveSearcher();
-    let disposable = commands.registerCommand('extension.sayHello', () => {
-        weaveSearcher.search();
+    let sayHelloCommand = commands.registerCommand('extension.sayHello', () => {
+        weaveSearcher.searchBar();
     });
+    // let searchThisCommand = commands.registerCommand('extension.searchThis', (text) => {
+    //     weaveSearcher.search(text);
+    // });
     context.subscriptions.push(weaveSearcher);
-    context.subscriptions.push(disposable);
+    context.subscriptions.push(sayHelloCommand);
+    // context.subscriptions.push(searchThisCommand);
 }
