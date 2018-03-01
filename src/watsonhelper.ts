16fc3edb0d8e6d10f9688e870840ba9c8e27dbc3
Michael Bayless <bayless.47@osu.edu>
2018-03-01 18:08:58 -0500

    This should work

--- src/watsonhelper.ts
+++ src/watsonhelper.ts
@@ -3,17 +3,30 @@ import * as WebRequest from 'web-request';
 
 export class WatsonHelper {
 
-    public searchWatson(searchText: string): Promise<WebRequest.Response<string>> {
-        
-        // TODO: move away from URL/WebRequests -> Watson Node library
-        var url = "https://gateway.watsonplatform.net/discovery/api/v1/environments/3120b03d-ac9c-46ac-a5e8-eaa282965961/collections/9ccfd375-fb1b-45c8-91d5-c3b2128e8038/query?version=2017-11-07&count=5&query=" + searchText;
-        var headers = this._buildWatsonHeaders(searchText);
+    public searchDiscovery(searchText: string): Promise<WebRequest.Response<string>> {
+        var url = "https://gateway.watsonplatform.net/discovery/api/v1/environments/" +
+            "3120b03d-ac9c-46ac-a5e8-eaa282965961/collections/9ccfd375-fb1b-45c8-91d5-c3b2128e8038/" +
+            "query?version=2017-11-07&count=5&query=" + searchText;
+        var headers = this._buildDiscoveryHeaders(searchText);
         var result = WebRequest.json<any>(url, headers);
         return result;
     }
 
-    private _buildWatsonHeaders(query: string): any {
+    private _buildDiscoveryHeaders(query: string): any {
         var credential = require('../env/discovery_credential.json');
+        return credential;
+    }
+
+    public hitNLC(text: string): Promise<WebRequest.Response<string>> {
+        var url = "https://gateway.watsonplatform.net/natural-language-classifier/api/v1/classifiers/" +
+        "8fc642x299-nlc-1245/classify?text=" + text;
+        var headers = this._buildNLCHeaders(text);
+        var result = WebRequest.json<any>(url, headers);
+        return result;
+    }
+
+    private _buildNLCHeaders(query: string): any {
+        var credential = require('../env/nlc_credential.json');
         return credential;
     }
 
