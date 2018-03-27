f5d06d2d5bc8824faa02ea701535f9db9ac9fbaa
wesdarvin <wesdarvin@gmail.com>
2018-03-27 18:12:28 -0400

    Correct document parsing for psuedo-code error

--- src/documenthelper.ts
+++ src/documenthelper.ts
@@ -22,6 +22,9 @@ export class DocumentHelper {
         var firstPart = document.substring(0,first);
         var start = firstPart.lastIndexOf("\n");
         var end = document.indexOf("Pseudocode");
+        if (end == -1) {
+            end = document.indexOf("Pseudo-code");
+        }
         var stephen = document.substring(start,end);
         stephen = stephen.substring(0,stephen.lastIndexOf("\n"));
         return stephen;
@@ -29,6 +32,9 @@ export class DocumentHelper {
 
     public Parse2TabPseudo(document: string): string {
         var first = document.indexOf("Pseudocode");
+        if (first == -1) {
+            first = document.indexOf("Pseudo-code");
+        }
         var firstPart = document.substring(0,first);
         var start = firstPart.lastIndexOf("\n");
         var end = document.indexOf("Code");
